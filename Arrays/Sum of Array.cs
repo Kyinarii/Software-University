4dbf31c4e96d6e078bfd4bce08d2d4ff8772ce86
Krasimir Angelov <134134494+Kyinarii@users.noreply.github.com>
2024-03-10 09:42:04 +0200

    Update Sum of Array.cs

--- Arrays/Sum of Array.cs
+++ Arrays/Sum of Array.cs
@@ -1,1 +1,16 @@
+using System.Globalization;
+
+int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+
+Console.WriteLine(sum);
 
